031f96a59184af0688c0aa3a528a7982239f080b
Dharmik-Pavasiya <dharmikpavasiya808@gmail.com>
2022-08-06 16:31:48 +0530

    fetch user data

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:intl/intl.dart';
 import './model/transaction_model.dart';
 
 void main() => runApp(MyApp());
@@ -7,9 +8,17 @@ class MyApp extends StatelessWidget {
   MyApp({Key? key}) : super(key: key);
 
   List<TranscationModel> transcations = [
-    TranscationModel(id: '01', title: 'New Shoes', amount: 1600, date: DateTime.now()),
-    TranscationModel(id: '02', title: 'New Shocks', amount: 150, date: DateTime.now()),
+    TranscationModel(
+        id: '01', title: 'New Shoes', amount: 160, date: DateTime.now()),
+    TranscationModel(
+        id: '02', title: 'New Shocks', amount: 150, date: DateTime.now()),
   ];
+  //
+  // String? titleInput;
+  // String? amountInput;
+
+  final titleController = TextEditingController();
+  final amountController = TextEditingController();
 
   @override
   Widget build(BuildContext context) {
@@ -21,17 +30,91 @@ class MyApp extends StatelessWidget {
           title: const Text('Material App Bar'),
         ),
         body: Column(
-          children: transcations.map(
-              (tx){
+          // mainAxisAlignment: MainAxisAlignment.spaceAround,
+          crossAxisAlignment: CrossAxisAlignment.stretch,
+          children: [
+            Container(
+              child: const Card(
+                child: Text('CHART !'),
+              ),
+            ),
+            Card(
+              elevation: 5,
+              child: Container(
+                padding: const EdgeInsets.all(10),
+                child: Column(
+                  crossAxisAlignment: CrossAxisAlignment.end,
+                  children: [
+                    TextField(
+                      decoration: const InputDecoration(
+                        labelText: 'Title',
+                      ),
+                      controller: titleController,
+                      // onChanged: (value){
+                      //   titleInput = value;
+                      // },
+                    ),
+                    TextField(
+                      decoration: const InputDecoration(
+                        labelText: 'Amount',
+                      ),
+                      controller: amountController,
+                      // onChanged: (value){
+                      //   amountInput = value;
+                      // },
+                    ),
+                    FlatButton(
+                      onPressed: () {
+                        print(titleController.text);
+                        print(amountController.text);
+                      },
+                      textColor: Colors.purple,
+                      child: const Text('Add Trasaction'),
+                    )
+                  ],
+                ),
+              ),
+            ),
+            Column(
+              children: transcations.map((tx) {
                 return Card(
-                  child: ListTile(
-                    leading: Text('${tx.amount}'),
-                    title: Text(tx.title),
-                    trailing: Text('${tx.date}'),
+                  child: Row(
+                    children: [
+                      Container(
+                        margin: const EdgeInsets.symmetric(
+                            horizontal: 15, vertical: 10),
+                        decoration: BoxDecoration(
+                            border: Border.all(color: Colors.purple, width: 2)),
+                        padding: const EdgeInsets.all(10),
+                        child: Text(
+                          '₹ ${tx.amount.toString()}',
+                          style: const TextStyle(
+                              fontWeight: FontWeight.bold,
+                              fontSize: 20,
+                              color: Colors.purple),
+                        ),
+                      ),
+                      Column(
+                        crossAxisAlignment: CrossAxisAlignment.start,
+                        children: [
+                          Text(
+                            tx.title,
+                            style: const TextStyle(
+                                fontSize: 16, fontWeight: FontWeight.bold),
+                          ),
+                          Text(
+                            DateFormat.yMMMd().format(tx.date),
+                            style: TextStyle(
+                                fontSize: 14, color: Colors.grey.shade700),
+                          ),
+                        ],
+                      )
+                    ],
                   ),
                 );
-              }
-          ).toList(),
+              }).toList(),
+            ),
+          ],
         ),
       ),
     );
