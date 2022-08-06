55a93fb1e60278bdca24d73ac1abb65c457ccf2b
Dharmik-Pavasiya <dharmikpavasiya808@gmail.com>
2022-08-06 15:17:09 +0530

    initial commit

--- lib/main.dart
+++ lib/main.dart
@@ -1,22 +1,37 @@
 import 'package:flutter/material.dart';
+import './model/transaction_model.dart';
 
-void main() => runApp(const MyApp());
+void main() => runApp(MyApp());
 
 class MyApp extends StatelessWidget {
-  const MyApp({Key? key}) : super(key: key);
+  MyApp({Key? key}) : super(key: key);
+
+  List<TranscationModel> transcations = [
+    TranscationModel(id: '01', title: 'New Shoes', amount: 1600, date: DateTime.now()),
+    TranscationModel(id: '02', title: 'New Shocks', amount: 150, date: DateTime.now()),
+  ];
 
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
+      debugShowCheckedModeBanner: false,
       title: 'Material App',
       home: Scaffold(
         appBar: AppBar(
           title: const Text('Material App Bar'),
         ),
-        body: Center(
-          child: Container(
-            child: const Text('Hello World'),
-          ),
+        body: Column(
+          children: transcations.map(
+              (tx){
+                return Card(
+                  child: ListTile(
+                    leading: Text('${tx.amount}'),
+                    title: Text(tx.title),
+                    trailing: Text('${tx.date}'),
+                  ),
+                );
+              }
+          ).toList(),
         ),
       ),
     );
